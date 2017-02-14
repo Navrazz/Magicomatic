70cf6037eba773e40dbf0b6e4cce7359bf685bff
Piotr Kocia <navrazz@gmail.com>
2017-02-14 13:26:08 +0100

    Add FileManager

---
+++ Magicomatic/Magicomatic.Data.UnitTests/Tools/FileManagerTests.cs
@@ -0,0 +1,32 @@
+﻿
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using Magicomatic.Data.Tools;
+
+namespace Magicomatic.Data.UnitTests.Tools
+{
+    [TestFixture]
+    class FileManagerTests : AssertionHelper
+    {
+        private string FilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Data/EMN.csv";
+        private FileManager fileManager;
+
+        [SetUp]
+        public void Before_Each_Test()
+        {
+            this.fileManager = new FileManager();
+        }
+
+        [Test]
+        public void Reads_Lines_From_File()
+        {
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var actual = fileManager.ReadLines(FilePath);
+            var expected = File.ReadLines(FilePath);
+
+            Expect(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Magicomatic/Magicomatic.Data/Tools/FileManager.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Collections;
+
+namespace Magicomatic.Data.Tools
+{
+    public class FileManager
+    {
+        public virtual IEnumerable ReadLines(string path)
+        {
+            return File.ReadLines(path);
+        }
+    }
+}
