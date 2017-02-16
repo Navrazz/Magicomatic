e7f990a6957abb780c23207d9895cec229044bf7
Piotr Kocia <piotr.kocia@gmail.com>
2017-02-16 11:43:22 +0100

    Add application ui

---
+++ Magicomatic/Magicomatic.UI/Views/CardLibraryView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Magicomatic.UI.Views
+{
+    public partial class CardLibraryView : UserControl
+    {
+        public CardLibraryView()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //logic
+        }
+
+        private void dataGrid_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //logic
+        }
+
+    }
+}
